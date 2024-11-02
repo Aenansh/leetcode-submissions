74d810c65351eaffa8a66ec8fd144fb88d485b36
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2024-11-02 17:50:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (100.00%)

---
+++ solutions/2580-circular-sentence/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.length();
+        // for(int i = 0; i < n; i++)
+        // {
+        //     sentence[i] = toupper(sentence[i]);
+        // }
+        if(sentence[0] != sentence[n - 1])
+        return false;
+        for(int i = 0; i < n; i++)
+        {
+            if(sentence[i] == ' ')
+            {
+                if(sentence[i - 1] != sentence[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
