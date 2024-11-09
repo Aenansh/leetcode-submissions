bf939ccdb82eca42e733d756c7941b1881ae380f
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2024-11-09 08:21:05 +0000

    Sync LeetCode submission Runtime - 19 ms (6.49%), Memory - 24 MB (7.58%)

---
+++ solutions/0136-single-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(auto i : nums)
+        {
+            count[i]++;
+        }
+        for(auto i : count)
+        {
+            if(i.second == 1)
+            return i.first;
+        }
+        return 0;
+    }
+};
