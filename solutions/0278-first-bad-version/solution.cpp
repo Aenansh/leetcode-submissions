fae3a4621ba83bd2283f046dc1bcd15ec7e30aa1
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2024-11-19 07:45:11 +0000

    Sync LeetCode submission Runtime - 272 ms (1.94%), Memory - 7.5 MB (100.00%)

---
+++ solutions/0278-first-bad-version/solution.cpp
@@ -0,0 +1,13 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for(int i = 1; i <= n; i++)
+        {
+            if(isBadVersion(i)) return i;
+        }
+        return 0;
+    }
+};
