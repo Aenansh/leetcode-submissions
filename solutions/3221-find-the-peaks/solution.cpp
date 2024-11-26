172aba60b625d8f09796bac78137fe5bbb7b7053
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2024-11-26 08:16:07 +0000

    Sync LeetCode submission Runtime - 1 ms (13.40%), Memory - 24.6 MB (9.32%)

---
+++ solutions/3221-find-the-peaks/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> count;
+        for (int i = 1; i < mountain.size() - 1; i++) {
+            if (mountain[i - 1] < mountain[i] &&
+                mountain[i] > mountain[i + 1]) {
+                count.push_back(i);
+                i++;
+            }
+        }
+        return count;
+    }
+};
