9a83b1b4da7643dad5b5a8311c51dc03ccb7d217
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2024-12-31 09:02:50 +0000

    Sync LeetCode submission Runtime - 3 ms (19.62%), Memory - 71.8 MB (14.95%)

---
+++ solutions/0053-maximum-subarray/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum = INT_MIN;
+        int sum = 0;
+        for(auto i : nums){
+            sum += i;
+            maxsum = max(maxsum, sum);
+            if(sum < 0) sum = 0;
+        }
+        return maxsum;
+    }
+};
