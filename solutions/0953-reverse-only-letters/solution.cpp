8d6b4eb764c48a84c209bbc22e7ce6e248f7f0cc
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2025-02-02 05:07:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (53.18%)

---
+++ solutions/0953-reverse-only-letters/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int low = 0, high = s.size() - 1;
+        while(low <= high){
+            if(!isalpha(s[low])) low++;
+            if(!isalpha(s[high])) high--;
+            else if(isalpha(s[low]) && isalpha(s[high])){
+                swap(s[low], s[high]);
+                low++;
+                high--;
+            }
+        }
+        return s;
+    }
+};
