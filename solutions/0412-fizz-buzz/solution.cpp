935edf55b79c814865dc42c4e005343147d58cba
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2025-10-01 16:57:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (73.07%)

--- solutions/0412-fizz-buzz/solution.cpp
+++ solutions/0412-fizz-buzz/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> answer;
-        for(int i = 1; i <= n; i++)
-        {
-            if(i % 3 == 0 && i % 5 == 0)
-            {
-                answer.push_back("FizzBuzz");
-            }
-            else if(i % 3 == 0)
-            {
-                answer.push_back("Fizz");
-            }
-            else if(i % 5 == 0)
-            {
-                answer.push_back("Buzz");
-            }
-            else{
-                answer.push_back(to_string(i));
+        vector<string> ans;
+        for (int i = 0; i < n; i++) {
+            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
+                ans.push_back("FizzBuzz");
+            } else if ((i + 1) % 3 == 0)
+                ans.push_back("Fizz");
+            else if ((i + 1) % 5 == 0)
+                ans.push_back("Buzz");
+            else {
+                string s = to_string(i + 1);
+                ans.push_back(s);
             }
         }
-        return answer;
+        return ans;
     }
 };
