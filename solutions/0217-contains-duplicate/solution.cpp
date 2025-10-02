a26b00f941c469875a358e530872768652018cd9
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2025-10-02 17:43:08 +0000

    Sync LeetCode submission Runtime - 73 ms (13.40%), Memory - 92.9 MB (12.74%)

--- solutions/0217-contains-duplicate/solution.cpp
+++ solutions/0217-contains-duplicate/solution.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> store;
-        for(auto i : nums)
-        {
-            store.insert(i);
-        }
-        if(nums.size() != store.size())
-        return true;
-        return false;
+        unordered_set<int> s(nums.begin(), nums.end());
+        return s.size() != nums.size();
     }
 };
