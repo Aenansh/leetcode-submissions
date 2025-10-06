5ac0c9951d5b85419e008bee8e6eb9d210d4652a
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2025-10-06 17:32:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.6 MB (62.39%)

--- solutions/0118-pascals-triangle/solution.cpp
+++ solutions/0118-pascals-triangle/solution.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascal;
-        for (int i = 0; i < numRows; i++) {
+        vector<vector<int>> pasTri;
+        pasTri.push_back({1});
+
+        if(numRows == 1) return pasTri;
+
+        pasTri.push_back({1, 1});
+        
+        for(int i = 2; i < numRows; i++) {
             vector<int> row(i + 1, 1);
-                for (int j = 1; j < i; j++) {
-                    row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j];   
-                }
-            pascal.push_back(row);
+            for(int j = 0; j < i - 1; j++) {
+                row[j + 1] = pasTri[i - 1][j] + pasTri[i - 1][j + 1];
+            }
+            pasTri.push_back(row);
         }
-        return pascal;
+        return pasTri;
     }
 };
