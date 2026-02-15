e977c66b3471ee1337dbc86385243adbccb4b233
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-02-15 06:32:39 +0000

    Sync LeetCode submission Runtime - 1 ms (73.38%), Memory - 14.2 MB (54.55%)

--- solutions/1482-how-many-numbers-are-smaller-than-the-current-number/solution.cpp
+++ solutions/1482-how-many-numbers-are-smaller-than-the-current-number/solution.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int max = nums[0];
-        for (int i : nums) {
-            if (i > max)
-                max = i;
-        }
-        vector<int> cnt(max + 1, 0);
-        for (int i : nums) {
-            cnt[i]++;
-        }
-        for (int i = 1; i < cnt.size(); i++) {
-            cnt[i] += cnt[i - 1];
-        }
+        int big = *max_element(nums.begin(), nums.end());
+
+        vector<int> cnt(big + 1, 0);
+
+        for(auto i : nums) cnt[i]++;
+        for(int i = 1; i < cnt.size(); i++) cnt[i] += cnt[i - 1];
         vector<int> ans(nums.size(), 0);
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] == 0)
-                ans[i] = 0;
-            else {
-                int val = cnt[nums[i] - 1];
-                ans[i] = val;
-            }
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            if(nums[i] == 0) continue;
+            else ans[i] = cnt[nums[i] - 1];
         }
         return ans;
     }
