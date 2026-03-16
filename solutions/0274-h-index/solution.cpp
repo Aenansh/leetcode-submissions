52c096f986f3813311c364f910e932e3b9e5746b
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-03-16 18:46:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (98.91%)

---
+++ solutions/0274-h-index/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int l = 0, r = n - 1;
+        int res = 0;
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(citations[mid] >= n - mid) {
+                res = n - mid;
+                r = mid - 1;
+            } else l = mid + 1;
+        }
+        return res;
+    }
+};
