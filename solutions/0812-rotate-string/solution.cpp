314145a5a0c44de3a68af2be0d9331fa1d59b7ce
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-05-03 03:48:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (84.87%)

--- solutions/0812-rotate-string/solution.cpp
+++ solutions/0812-rotate-string/solution.cpp
@@ -1,15 +1,43 @@
+#define D 26
+#define MOD 5381
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if((s.length() <= 1 && goal.length() <= 1) && (s != goal))
-        return false;
-        if(s.length() != goal.length())
-        return false;
-        for(int i = 0; i < s.length(); i++)
-        {
-            if(s == goal)
-            return true;
-            s = s.substr(1, s.length()) + s[0];
+        if (s.length() != goal.length())
+            return false;
+        int n = s.length();
+
+        long long hash1 = 0;
+        long long hash2 = 0;
+        long long h = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            h = (h * D) % MOD;
+        }
+
+        for (int i = 0; i < n; i++) {
+            hash1 = (hash1 * D + (s[i] - 'a')) % MOD;
+            hash2 = (hash2 * D + (goal[i] - 'a')) % MOD;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (hash1 == hash2) {
+                bool isMatch = true;
+                for (int j = 0; j < n; j++) {
+                    if (s[(i + j) % n] != goal[j]) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch)
+                    return true;
+            }
+            int charToMove = s[i] - 'a';
+            hash1 = (hash1 - (charToMove * h)) % MOD;
+            if (hash1 < 0)
+                hash1 += MOD;
+            hash1 = (hash1 * D + charToMove) % MOD;
         }
         return false;
     }
