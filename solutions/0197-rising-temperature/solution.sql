5ca454bdda4544314f5323fb8af4dcf46b53e335
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-06-04 09:17:08 +0000

    Sync LeetCode submission Runtime - 550 ms (17.56%), Memory - 0.0B (100.00%)

--- solutions/0197-rising-temperature/solution.sql
+++ solutions/0197-rising-temperature/solution.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select today.id from weather as today
-where exists
-    (select 1 from weather as yesterday
-    where yesterday.temperature < today.temperature
-    and
-    datediff(today.recordDate, yesterday.recordDate) = 1)
+select today.id from
+weather as today
+inner join weather as yesterday
+where today.temperature > yesterday.temperature
+and datediff(today.recordDate, yesterday.recordDate) = 1;
