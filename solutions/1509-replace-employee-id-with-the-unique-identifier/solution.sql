a8c8443f5fd144adbbd68f6e1a79a1266af0c7af
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-06-04 07:13:59 +0000

    Sync LeetCode submission Runtime - 1241 ms (44.99%), Memory - 0.0B (100.00%)

--- solutions/1509-replace-employee-id-with-the-unique-identifier/solution.sql
+++ solutions/1509-replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select eu.unique_id, e.name
-from employees as e 
-left join employeeUNI as eu
+from employees as e
+left join employeeuni as eu
 on
 e.id = eu.id
