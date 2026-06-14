5a8716b30b19caf0139fc0d4f08093d2615435eb
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-06-14 04:53:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 36.3 MB (20.35%)

---
+++ solutions/0724-find-pivot-index/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefixSum(n, 0);
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int leftSum = prefixSum[i] - nums[i];
+            int rightSum = prefixSum[n - 1] - prefixSum[i];
+            if (leftSum == rightSum)
+                return i;
+        }
+        return -1;
+    }
+};
