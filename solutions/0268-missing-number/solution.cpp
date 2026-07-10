13cb53544752e0863622e93dc134acb3cf7b1db4
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-10 18:33:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.9 MB (38.66%)

--- solutions/0268-missing-number/solution.cpp
+++ solutions/0268-missing-number/solution.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int x1 = 0;
-        int x2 = 0;
         int n = nums.size();
-        for (auto i : nums)
-            x2 ^= i;
-        for (int i = 0; i < n + 1; i++)
-            x1 ^= i;
-        return x1 ^ x2;
+        long long sum = (1LL * n * (n + 1)) / 2;
+        for(int i : nums) sum -= i;
+        return sum;
     }
 };
