7906d449c6ee4d442de849dbe5bb41e1ce972503
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-11 04:32:26 +0000

    Sync LeetCode submission Runtime - 93 ms (11.39%), Memory - 140.1 MB (17.62%)

--- solutions/2793-count-the-number-of-complete-components/solution.cpp
+++ solutions/2793-count-the-number-of-complete-components/solution.cpp
@@ -1,22 +1,19 @@
 class DSU {
 public:
-    vector<int> parent;
-    vector<int> size;
-
+    vector<int> parent, size;
     DSU(int n) {
         parent.resize(n);
-        iota(parent.begin(), parent.end(), 0);
+        iota(begin(parent), end(parent), 0);
         size.resize(n, 1);
     }
-
-    int Find(int u) {
-        if (parent[u] == u)
+    int find(int u) {
+        if (u == parent[u])
             return u;
-        return parent[u] = Find(parent[u]);
+        return parent[u] = find(parent[u]);
     }
-    void Union(int u, int v) {
-        int pu = Find(u);
-        int pv = Find(v);
+    void unite(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
         if (pu == pv)
             return;
         if (size[pu] >= size[pv]) {
@@ -28,35 +25,33 @@ public:
         }
     }
 };
+
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        DSU obj(n);
         vector<vector<int>> adj(n);
+        DSU d(n);
         for (auto edge : edges) {
             int u = edge[0], v = edge[1];
+            d.unite(u, v);
             adj[u].push_back(v);
             adj[v].push_back(u);
-            obj.Union(u, v);
         }
 
         unordered_map<int, vector<int>> comps;
         for (int i = 0; i < n; i++) {
-            int root = obj.Find(i);
-            comps[root].push_back(i);
+            int pr = d.find(i);
+            comps[pr].push_back(i);
         }
-        
+
         int totalComps = comps.size();
 
-        for(auto [key, val] : comps) {
-            int vsize = val.size() - 1;
-            if(adj[key].size() < vsize) totalComps--;
-            else {
-                for(int node : val) {
-                    if(adj[node].size() < vsize) {
-                        totalComps--;
-                        break;
-                    }
+        for (auto [key, val] : comps) {
+            int vsz = val.size() - 1;
+            for (auto node : val) {
+                if (adj[node].size() < vsz) {
+                    totalComps--;
+                    break;
                 }
             }
         }
