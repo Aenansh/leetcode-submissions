01d9a1787843fc8e2097e576da7655faf2b39f55
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-19 18:10:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (81.73%)

---
+++ solutions/0316-remove-duplicate-letters/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> seen(26, false);
+        vector<int> lastOcc(26, -1);
+
+        int n = s.length();
+
+        string ans = "";
+
+        for (int i = 0; i < n; i++) {
+            lastOcc[s[i] - 'a'] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (!seen[ch - 'a']) {
+                while (!ans.empty() && ans.back() > ch) {
+                    char bck = ans.back();
+                    if (lastOcc[bck - 'a'] > i) {
+                        ans.pop_back();
+                        seen[bck - 'a'] = false;
+                    } else
+                        break;
+                }
+                ans.push_back(ch);
+                seen[ch - 'a'] = true;
+            }
+        }
+        return ans;
+    }
+};
