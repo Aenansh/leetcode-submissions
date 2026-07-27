dfabfeb1325e9a707fc4e6e001bb0c8b16826cda
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-27 04:01:47 +0000

    Sync LeetCode submission Runtime - 3 ms (15.46%), Memory - 13.6 MB (20.18%)

--- solutions/1574-maximum-product-of-two-elements-in-an-array/solution.cpp
+++ solutions/1574-maximum-product-of-two-elements-in-an-array/solution.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        make_heap(nums.begin(), nums.end());
-        int first = nums.front();
-        pop_heap(nums.begin(), nums.end());
-        return (nums.front() - 1) * (first - 1);
+        sort(nums.begin(), nums.end(), greater<int>());
+        return (nums[0] - 1) * (nums[1] - 1);
     }
 };
