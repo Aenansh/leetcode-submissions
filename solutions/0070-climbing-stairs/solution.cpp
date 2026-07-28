5caa1e31a5d3a92bda77037992d90e17ad50d4f1
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-28 07:04:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (74.41%)

--- solutions/0070-climbing-stairs/solution.cpp
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n < 1)
-        {
-            return 0;
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int temp = a;
+            a = b;
+            b = temp + b;
         }
-        else if(n <= 2)
-        {
-            return n;
-        }
-        int k = 1, m = 2, sum = 0;
-        for(int i = 3; i <= n; i++)
-        {
-            sum = k + m;
-            k = m;
-            m = sum;
-        }
-        return sum;
+        return b;
     }
 };
