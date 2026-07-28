f2a98254338012ac5a652b366e619249d8747b2e
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-28 11:18:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15 MB (67.20%)

--- solutions/0967-minimum-falling-path-sum/solution.cpp
+++ solutions/0967-minimum-falling-path-sum/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
-    int solve(vector<vector<int>>& dp, vector<vector<int>>& matrix, int i,
-              int j) {
-        if (j < 0 || j >= matrix[0].size())
-            return 1e9;
-        if (i == (matrix.size() - 1))
-            return matrix[i][j];
-
-        if (dp[i][j] != 10005)
-            return dp[i][j];
-
-        int minf = 1e9;
-        int db = solve(dp, matrix, i + 1, j);
-        int dl = solve(dp, matrix, i + 1, j - 1);
-        int dr = solve(dp, matrix, i + 1, j + 1);
-
-        minf = matrix[i][j] + min({dl, dr, db});
-        return dp[i][j] = minf;
-    }
-
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, 10005));
-        int minf = 1e9;
-        for (int i = 0; i < n; i++)
-            minf = min(minf, solve(dp, matrix, 0, i));
-        return minf;
+
+        vector<int> upperRow = matrix[0];
+
+        int d[3] = {1, 0, -1};
+
+        for (int i = 1; i < n; i++) {
+            vector<int> currRow = matrix[i];
+            for (int j = 0; j < n; j++) {
+                int minFall = INT_MAX;
+                for (int k = 0; k < 3; k++) {
+                    int idx = j + d[k];
+                    if (idx >= 0 && idx < n) {
+                        minFall = min(minFall, upperRow[idx]);
+                    }
+                }
+                currRow[j] += minFall;
+            }
+            upperRow = currRow;
+        }
+
+        return *min_element(begin(upperRow), end(upperRow));
     }
 };
