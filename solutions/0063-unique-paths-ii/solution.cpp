575c4e0a45ee6a31e0d8cab6baba93b3e68ca618
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-28 10:52:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (79.22%)

--- solutions/0063-unique-paths-ii/solution.cpp
+++ solutions/0063-unique-paths-ii/solution.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0] == 1)
+            return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> table(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1)
-                    table[i][j] = 0;
 
-                else {
-                    if (i - 1 >= 0 && j - 1 >= 0) {
-                        table[i][j] = table[i - 1][j] + table[i][j - 1];
-                    }
-                    else if(i - 1 < 0 && j - 1 >= 0) {
-                        table[i][j] = table[i][j - 1];
-                    }
-                    else if(j - 1 < 0 && i - 1 >= 0) {
-                        table[i][j] = table[i - 1][j];
-                    }
-                    else {
-                        table[i][j] = 1;
-                    }
+        vector<int> upperRow(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 0)
+                upperRow[i] = 1;
+            else
+                break;
+        }
+
+        for (int i = 1; i < m; i++) {
+            vector<int> currRow(n, 0);
+            if (obstacleGrid[i][0] == 0)
+                currRow[0] = upperRow[0];
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    currRow[j] = currRow[j - 1] + upperRow[j];
                 }
             }
+            upperRow = currRow;
         }
-        return table[m - 1][n - 1];
+
+        return upperRow.back();
     }
 };
