2a2d7b170505bcd19710d5ba31259e5aa4b3c967
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-07-28 07:05:37 +0000

    Sync LeetCode submission Runtime - 2 ms (77.90%), Memory - 7.8 MB (38.78%)

--- solutions/1013-fibonacci-number/solution.cpp
+++ solutions/1013-fibonacci-number/solution.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n < 1)
-        {
-            return 0;
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = a;
+            a = b;
+            b = b + temp;
         }
-        else if(n == 1)
-        {
-            return 1;
-        }
-        int pre1 = 0, pre2 = 1, curr = 0;
-        for(int i = 1; i < n; i++)
-        {
-            curr = pre1 + pre2;
-            pre1 = pre2;
-            pre2 = curr;
-        }
-        return curr;
+        return a;
     }
 };
