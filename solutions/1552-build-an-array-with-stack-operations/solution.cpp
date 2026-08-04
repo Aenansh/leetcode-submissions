bf01cdb5556856d8eb09416b402cb7c7c28a1cdc
Aenansh Mittal <147971476+Aenansh@users.noreply.github.com>
2026-08-04 17:57:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.4 MB (6.04%)

--- solutions/1552-build-an-array-with-stack-operations/solution.cpp
+++ solutions/1552-build-an-array-with-stack-operations/solution.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> oper;
-        for (int i = 1; i <= target[target.size() - 1]; i++) {
-            oper.push_back("Push");
-            if (find(target.begin(), target.end(), i) == target.end())
-                oper.push_back("Pop");
-            // else
-            //     oper.push_back("Push");
+        vector<string> ans;
+
+        unordered_map<int, bool> mp;
+
+        for (auto t : target)
+            mp[t] = true;
+
+        stack<int> s;
+        int idx = 0, i = 1;
+        int ts = target.size();
+
+        while (i <= n && idx < ts) {
+            if (mp[i]) {
+                idx++;
+                while (!s.empty() && !mp[s.top()]) {
+                    s.pop();
+                    ans.push_back("Pop");
+                }
+            }
+            s.push(i++);
+            ans.push_back("Push");
         }
-        return oper;
+
+        return ans;
     }
 };
